b2037f5c655b9bcf915a92d682067f636939c90a
cedric816 <73229376+cedric816@users.noreply.github.com>
2020-12-04 12:54:21 +0100

    Add files via upload

---
+++ popup.php
@@ -0,0 +1,15 @@
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>wax-addict — boutique officielle</title>
+  <meta name="description" content="Bienvenue dans la boutique privée “Wax-Addict”. Retrouvez notre toute dernière sélection de tissus africains hauts de gamme.">
+  <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div id="popup-container">
+        <div id="close-popup"><a id="btn-close-popup" href="back-office--template.php">X</a></div>
+        <img id="img-popup" src="<?php echo $_GET['url'] ?>">
+    </div>
+</body>
